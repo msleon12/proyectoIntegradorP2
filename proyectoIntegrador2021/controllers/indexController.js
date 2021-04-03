22a4fecf1dc91315681ee9aec4c88fd6b94868b2
msleon12 <martinleon2013@gmail.com>
2021-04-03 16:52:59 -0300

    avance en la vista de index

--- proyectoIntegrador2021/controllers/indexController.js
+++ proyectoIntegrador2021/controllers/indexController.js
@@ -2,7 +2,7 @@ const products = require('../data/info')
 
 const indexController = {
     index: function(req,res){
-        return res.render('index', {title: 'Index'})
+        return res.render('index', {title: 'Index', products: products.perfumes, products2: products.perfumes2})
     }
 
 }
--- proyectoIntegrador2021/data/info.js
+++ proyectoIntegrador2021/data/info.js
@@ -4,12 +4,68 @@ const products = {
         nombre: '212',
         marca: 'Carolina Herrera',
         ml: 40, 
-        anio: 2014 
+        anio: 2014,
+        descripcion: "eaeaeasasa",
+
     }, 
     {
         id: 2, 
+        nombre: 'One Million',
+        marca: 'Giorgio armani',
+        ml: 60, 
+        anio: 2017,
+        descripcion: "lorems",
+    },
+    {
+        id: 3, 
+        nombre: 'rered',
+        marca: 'Love',
+        ml: 60, 
+        anio: 2017,
+        descripcion: "hola",
+    },
+    {
+        id: 4, 
+        nombre: 'Invictus',
+        marca: 'Love',
+        ml: 80, 
+        anio: 2017,
+        descripcion: "hola",
+    }],
+    perfumes2: [{
+        id:1,
+        nombre: "Klein",
+        marca: 'Calvin Klein',
+        ml: 40, 
+        anio: 2014,
+        descripcion: "dsdsdfs",
+    },
+    {
+        id:2,
+        nombre: "Klein",
+        marca: 'Calvin Klein',
+        ml: 40, 
+        anio: 2014,
+        descripcion: "dsdsdfs",
+    },
+    {
+        id:3,
+        nombre: "Klein",
+        marca: 'Calvin Klein',
+        ml: 40, 
+        anio: 2014,
+        descripcion: "dsdsdfs",
+    },
+    {
+        id:4,
+        nombre: "Klein",
+        marca: 'Calvin Klein',
+        ml: 40, 
+        anio: 2014,
+        descripcion: "dsdsdfs",
     },
 ]
+
 }
 
 module.exports = products
